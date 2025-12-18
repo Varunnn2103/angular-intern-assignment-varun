141348f0f65de1a73f003160ebf20ad468d1b9a7
Varunnn2103 <kalyadapuvarun21@gmail.com>
2025-12-18 09:19:07 +0530

    implement PostService with methods to fetch and create posts

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,34 @@ import { Injectable } from '@angular/core';
 export class PostService {
 
   constructor() { }
+}
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+ // pojo post interface
+export interface Post {
+  userId: number;
+  id?: number;
+  title?: string;
+  body?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) { }
+
+  // 1. Fetching posts Method(GET)
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.apiUrl);
+  }
+
+  // 2. Creating the post method (POST)
+  createPost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.apiUrl, post);
+  }
 }
